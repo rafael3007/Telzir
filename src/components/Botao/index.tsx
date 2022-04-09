9a8f6a83ce69be6c68d73009d90d293a51713f4f
rafael3007 <rafael.brito.14222@gmail.com>
2022-04-09 12:55:54 -0300

    styles

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './style.scss'
 
 export default function Botao(){
     return(
-        <button>
+        <button className="botao">
             Botão
         </button>
     )
--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Botao from "../Botao";
+import './style.scss'
 
 export default function Formulario(){
     return(
-        <form>
-            <div>
+        <form className="novaTarefa">
+            <div className="inputContainer">
                 <label htmlFor="tarefa">Adicione um novo estudo</label>
                 <input 
                     type={"text"}
@@ -14,7 +15,7 @@ export default function Formulario(){
                     required
                 />
             </div>
-            <div>
+            <div className="inputContainer">
                 <label htmlFor="tempo">Tempo</label>
                 <input 
                     type={"time"} 
